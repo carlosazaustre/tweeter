7de895e75976961ef920207a7d6ce03ef1aa8852
Carlos Azaustre <cazaustre@gmail.com>
2020-09-28 18:52:08 +0200

    added Icon component

---
+++ components/lib/atoms/Icon/helpers.js
@@ -0,0 +1,38 @@
+const ICONS_ASSETS_PATH = '/icons'
+
+const iconTypes = {
+  account: 'account_circle',
+  logout: 'exit_to_app',
+  close: 'clear',
+  comment: 'chat_bubble_outline',
+  retweet: 'loop',
+  favoriteOutline: 'favorite_outline',
+  favoriteFill: 'favorite',
+  bookmarkOutline: 'bookmark_outline',
+  bookmarkFill: 'bookmark',
+  picture: 'add_photo_alternate',
+  pictureAdd: 'camera_alt',
+  angleDown: 'arrow_drop_down',
+  contactAdd: 'person_add',
+  contact: 'person',
+  contactGroup: 'group',
+  contactOk: 'how_to_reg',
+  privateOutline: 'lock_outline',
+  privateFill: 'https',
+  public: 'public',
+  mail: 'mail_outline',
+  explore: 'explore',
+  home: 'home',
+  facebook: 'facebook',
+  google: 'google',
+  twitter: 'twitter',
+  github: 'github',
+}
+
+const iconSizes = {
+  sm: 13,
+  md: 16,
+}
+
+export const mapType = (type) => `${ICONS_ASSETS_PATH}/${iconTypes[type]}.svg`
+export const mapSize = (size) => iconSizes[size]
---
+++ components/stories/atoms/Icon.stories.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Icon from '../../lib/atoms/Icon'
+
+export default {
+  title: 'Atoms/Icon',
+  component: Icon,
+}
+
+const Template = (props) => <Icon {...props} />
+
+export const Default = Template.bind({})
+Default.args = {
+  type: 'public',
+  size: 'md',
+}
